515a2bdb4ffc02f58ad71acaee352b3de55e7bc2
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-06 19:00:41 +0000

    concat function in the string

--- src/app.js
+++ src/app.js
@@ -1,33 +1,15 @@
-window.onload = function() {
-  //write your code here
-  document.querySelector("#excuse").innerHTML = excusegenerator();
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = theExcuse;
 };
 
-//Served Array
-let excusegenerator = () => {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [ "before the class", "right on time", "when I finished", "during my lunch", "while I was praying" ];
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying"
+];
 
-  // let quien = Math.floor(Math.random() * who.length);
-  let quien = getRandomElement(who);
-  let accion = getRandomElement(action);
-  let que = getRandomElement(what);
-  let cuando = getRandomElement(when);
+let theExcuse = `${getElement(who)} ${getElement(action)} ${getElement(what)} ${getElement(when)}`;
 
-  return (
-    who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
-  );
-};
-
-let refresh = document.getElementById("refresh");
-refresh.addEventListener("click", _ => {
-  location.reload();
-});
-
-//Function to loop ramdon word
-function getRandomElement(ArrayCalcular) {
-  console.log("getRandomElement");
-  return Math.floor(Math.random() * ArrayCalcular.length);
+function getElement(array) {
+  return array[Math.floor(Math.random() * array.length)]; 
 }
